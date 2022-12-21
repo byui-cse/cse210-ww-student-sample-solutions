bc46d9c7a00417904beeb576d9e6cd3adc442839
Scott Burton <burtons@byui.edu>
2022-12-20 18:23:07 -0700

    Added period

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -10,6 +10,6 @@ class Program
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last}, {first} {last}");
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }

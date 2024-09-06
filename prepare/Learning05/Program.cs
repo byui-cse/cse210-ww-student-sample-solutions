62c2a4faba865d4f08f8ba0f24c2ce50fc36b8cb
alex <alexcchristensen93@gmail.com>
2024-09-05 23:14:46 -0600

    Moving week 04 learning to week 05, and week 05 to week 06. (no more learning activity for week 4, as it is a foundational review week)

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,30 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Notice that the list is a list of "Shape" objects. That means
-        // you can put any Shape objects in there, and also, any object where
-        // the class inherits from Shape
-        List<Shape> shapes = new List<Shape>();
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
 
-        Square s1 = new Square("Red", 3);
-        shapes.Add(s1);
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
 
-        Rectangle s2 = new Rectangle("Blue", 4, 5);
-        shapes.Add(s2);
-
-        Circle s3 = new Circle("Green", 6);
-        shapes.Add(s3);
-
-        foreach (Shape s in shapes)
-        {
-            // Notice that all shapes have a GetColor method from the base class
-            string color = s.GetColor();
-
-            // Notice that all shapes have a GetArea method, but the behavior is
-            // different for each type of shape
-            double area = s.GetArea();
-
-            Console.WriteLine($"The {color} shape has an area of {area}.");
-        }
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
